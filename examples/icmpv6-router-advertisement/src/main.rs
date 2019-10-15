ca99b740b40100c4b067a232ace199aeb251ec54
srolli047 <Stephen_Rollins@comcast.com>
2019-10-14 21:57:09 -0400

    format rust code

--- examples/icmpv6-router-advertisement/src/main.rs
+++ examples/icmpv6-router-advertisement/src/main.rs
@@ -2,10 +2,14 @@
 extern crate netbricks;
 extern crate fallible_iterator;
 
+use fallible_iterator::FallibleIterator;
 use netbricks::common::Result;
 use netbricks::config::load_config;
 use netbricks::interface::{PacketRx, PacketTx};
 use netbricks::operators::{Batch, ReceiveBatch};
+use netbricks::packets::icmp::v6::ndp::NdpPacket;
+use netbricks::packets::icmp::v6::Icmpv6Message;
+use netbricks::packets::icmp::v6::Icmpv6Parse;
 use netbricks::packets::icmp::v6::{Icmpv6, PacketTooBig};
 use netbricks::packets::ip::v6::{Ipv6, Ipv6Packet, IPV6_MIN_MTU};
 use netbricks::packets::ip::ProtocolNumbers;
@@ -13,10 +17,6 @@ use netbricks::packets::{EtherTypes, Ethernet, EthernetHeader, Fixed, Packet};
 use netbricks::runtime::Runtime;
 use netbricks::scheduler::Scheduler;
 use std::fmt::Display;
-use netbricks::packets::icmp::v6::Icmpv6Message;
-use netbricks::packets::icmp::v6::Icmpv6Parse;
-use netbricks::packets::icmp::v6::ndp::NdpPacket;
-use fallible_iterator::FallibleIterator;
 
 fn install<T, S>(ports: Vec<T>, sched: &mut S)
 where
@@ -30,7 +30,7 @@ where
         .map(|port| {
             ReceiveBatch::new(port.clone())
                 .map(|p| p.parse::<Ethernet>())
-                .filter(| p| p.ether_type() == EtherTypes::Ipv6)
+                .filter(|p| p.ether_type() == EtherTypes::Ipv6)
                 .group_by(
                     |ipv6| ipv6.next_header(),
                     |groups| {
@@ -59,19 +59,15 @@ where
     }
 }
 
-
 pub fn process_icmpv6(ipv6: Ipv6) -> Result<Option<Ipv6>> {
     if let Ok(Icmpv6Message::RouterAdvertisement(advert)) = ipv6.parse_icmpv6() {
         let mut iter = advert.options();
         while let Ok(Some(option)) = iter.next() {
-            if let NdpOptions::SourceLinkLayerAddress(option_type) = option {
-
-            }
+            if let NdpOptions::SourceLinkLayerAddress(option_type) = option {}
         }
     }
     Ok(None)
 }
-
 
 fn main() -> Result<()> {
     let configuration = load_config()?;
