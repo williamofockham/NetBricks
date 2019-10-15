5ca5a6153542ac2546d32adf62736d55fbf9976a
srolli047 <Stephen_Rollins@comcast.com>
2019-10-15 09:42:31 -0400

    update out file

--- examples/icmpv6-router-advertisement/check.sh
+++ examples/icmpv6-router-advertisement/check.sh
@@ -3,8 +3,8 @@ TEST_NAME=icmpv6-router-advertisement
 
 PORT_OPTIONS="dpdk:eth_pcap0,rx_pcap=data/icmpv6_router_advertisement.pcap,tx_pcap=/tmp/out.pcap"
 
-../../build.sh run $TEST_NAME -p $PORT_OPTIONS -c 0 -c 1 -d 1
-tcpdump -tner /tmp/out.pcap | tee /dev/tty | diff - data/expect_icmpv6_router_advertisement.out
+../../build.sh run $TEST_NAME -p $PORT_OPTIONS -c 0 -d 1
+tcpdump -tner /tmp/out.pcap | tee /dev/tty | diff - data/expect_icmpv6.out
 
 result=$?
 echo ----
