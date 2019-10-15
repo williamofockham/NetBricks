08ffcd0d273a6263372fca4aadd38bb1f2f12bab
srolli047 <Stephen_Rollins@comcast.com>
2019-10-14 23:03:36 -0400

    fix compile errors

--- examples/icmpv6-router-advertisement/src/main.rs
+++ examples/icmpv6-router-advertisement/src/main.rs
@@ -7,13 +7,13 @@ use netbricks::common::Result;
 use netbricks::config::load_config;
 use netbricks::interface::{PacketRx, PacketTx};
 use netbricks::operators::{Batch, ReceiveBatch};
-use netbricks::packets::icmp::v6::ndp::NdpPacket;
+use netbricks::packets::icmp::v6::ndp::{NdpOptions, NdpPacket};
 use netbricks::packets::icmp::v6::Icmpv6Message;
 use netbricks::packets::icmp::v6::Icmpv6Parse;
-use netbricks::packets::icmp::v6::{Icmpv6, PacketTooBig};
-use netbricks::packets::ip::v6::{Ipv6, Ipv6Packet, IPV6_MIN_MTU};
+use netbricks::packets::ip::v6::Ipv6;
+use netbricks::packets::ip::v6::Ipv6Packet;
 use netbricks::packets::ip::ProtocolNumbers;
-use netbricks::packets::{EtherTypes, Ethernet, EthernetHeader, Fixed, Packet};
+use netbricks::packets::{EtherTypes, Ethernet, Packet};
 use netbricks::runtime::Runtime;
 use netbricks::scheduler::Scheduler;
 use std::fmt::Display;
@@ -31,15 +31,15 @@ where
             ReceiveBatch::new(port.clone())
                 .map(|p| p.parse::<Ethernet>())
                 .filter(|p| p.ether_type() == EtherTypes::Ipv6)
+                .map(|p| p.parse::<Ipv6>())
                 .group_by(
                     |ipv6| ipv6.next_header(),
                     |groups| {
                         compose! {
                             groups,
                             ProtocolNumbers::Icmpv6 => |group| {
-                                group.filter_map(process_icpv6).emit()
+                                group.filter_map(process_icmpv6).emit()
                             },
-
                             _ => |group| {
                                 group.filter(|_| {
                                     false
@@ -63,7 +63,9 @@ pub fn process_icmpv6(ipv6: Ipv6) -> Result<Option<Ipv6>> {
     if let Ok(Icmpv6Message::RouterAdvertisement(advert)) = ipv6.parse_icmpv6() {
         let mut iter = advert.options();
         while let Ok(Some(option)) = iter.next() {
-            if let NdpOptions::SourceLinkLayerAddress(option_type) = option {}
+            if let NdpOptions::SourceLinkLayerAddress(_option_type) = option {
+                //process options here
+            }
         }
     }
     Ok(None)
